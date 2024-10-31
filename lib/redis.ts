a24b88446b35b9366ec64de36ac3c46c0b79fff7
KaalaAadmi <arnav264@gmail.com>
2024-10-31 14:02:42 +0000

    fixed deployment errors

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,24 +1,24 @@
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-// Initialize Redis client
+// // Initialize Redis client
 
-const redis = createClient({
-  password: "bZTJ1FXzzL4sG5nqXh4LLcKgUcDvHkPb",
-  socket: {
-    host: "redis-18627.c328.europe-west3-1.gce.redns.redis-cloud.com",
-    port: 18627,
-  },
-});
+// const redis = createClient({
+//   password: "bZTJ1FXzzL4sG5nqXh4LLcKgUcDvHkPb",
+//   socket: {
+//     host: "redis-18627.c328.europe-west3-1.gce.redns.redis-cloud.com",
+//     port: 18627,
+//   },
+// });
 
-// Handle connection errors
-redis.on("error", (err) => console.error("Redis Client Error", err));
+// // Handle connection errors
+// redis.on("error", (err) => console.error("Redis Client Error", err));
 
-// Connect the client if it's not already connected
-async function connectRedis() {
-  if (!redis.isOpen) {
-    await redis.connect();
-  }
-}
+// // Connect the client if it's not already connected
+// async function connectRedis() {
+//   if (!redis.isOpen) {
+//     await redis.connect();
+//   }
+// }
 
-// Export both the client and the connect function
-export { redis, connectRedis };
+// // Export both the client and the connect function
+// export { redis, connectRedis };
